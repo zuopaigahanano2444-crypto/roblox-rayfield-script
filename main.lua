4eda25234f052a9fb572edf74a43223a3bebfa63
zuopaigahanano2444-crypto <264237449+zuopaigahanano2444-crypto@users.noreply.github.com>
2026-03-03 12:32:10 -0500

    Update: Added Anti-Kick functionality

--- main.lua
+++ main.lua
@@ -1,32 +1,39 @@
+-- Anti-Kick Functionality (Must be at the top)
+local mt = getrawmetatable(game)
+local old = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+   local method = getnamecallmethod()
+   if method == "Kick" or method == "kick" then
+      return nil
+   end
+   return old(self, ...)
+end)
+
+setreadonly(mt, true)
+
+-- Rayfield UI Library
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Manus Script - Roblox",
+   Name = "Manus Script - Roblox (Anti-Kick)",
    LoadingTitle = "Rayfield UI Library",
    LoadingSubtitle = "by Manus",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = nil, -- Create a custom folder for your hub/game
+      FolderName = nil,
       FileName = "ManusHub"
    },
    Discord = {
       Enabled = false,
-      Invite = "noinvite", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+      Invite = "noinvite",
+      RememberJoins = true
    },
-   KeySystem = false, -- Set this to true to use their key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted 
-   }
+   KeySystem = false
 })
 
-local Tab = Window:CreateTab("Main Settings", 4483362458) -- Title, Image
+local Tab = Window:CreateTab("Main Settings", 4483362458)
 
 local Section = Tab:CreateSection("Character Modifiers")
 
@@ -36,7 +43,7 @@ local SliderSpeed = Tab:CreateSlider({
    Increment = 1,
    Suffix = "Speed",
    CurrentValue = 16,
-   Flag = "SliderSpeed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "SliderSpeed",
    Callback = function(Value)
       game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
    end,
@@ -48,18 +55,22 @@ local SliderJump = Tab:CreateSlider({
    Increment = 1,
    Suffix = "Power",
    CurrentValue = 50,
-   Flag = "SliderJump", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "SliderJump",
    Callback = function(Value)
       game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
    end,
 })
 
+local Section2 = Tab:CreateSection("Security")
+
+Tab:CreateLabel("Anti-Kick is automatically enabled.")
+
 Rayfield:Notify({
    Title = "Script Loaded",
-   Content = "Manus Script has been successfully loaded!",
+   Content = "Manus Script with Anti-Kick has been successfully loaded!",
    Duration = 6.5,
    Image = 4483362458,
-   Actions = { -- Notification Buttons
+   Actions = {
       Ignore = {
          Name = "Okay!",
          Callback = function()
